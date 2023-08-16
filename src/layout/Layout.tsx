174ec45f324fbc58d9c9b756fa89076322393a89
Jayson Acosta <36345453+jaysonacosta@users.noreply.github.com>
2023-08-16 11:54:13 -0400

    Added dropdown menu and sign out

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -7,12 +7,37 @@ import {
   IconUserCircle,
 } from "@tabler/icons-react";
 import Link from "next/link";
+import { useEffect, useRef, useState } from "react";
+
+import { signOut } from "next-auth/react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const [isDropdownVisible, setIsDropdownVisible] = useState(false);
+
+  const handleOnMouseClick = () => {
+    setIsDropdownVisible((prev) => !prev);
+  };
+
+  const ref = useRef<HTMLLIElement>(null);
+
+  useEffect(() => {
+    const handleOutsideClick = (evt: MouseEvent) => {
+      if (ref.current && !ref.current.contains(evt.target as Node)) {
+        setIsDropdownVisible(false);
+      }
+    };
+
+    document.addEventListener("click", handleOutsideClick);
+
+    return () => {
+      document.removeEventListener("click", handleOutsideClick);
+    };
+  }, [ref]);
+
   return (
     <>
-      <nav className="bg-gradient-to-r from-blue-200 to-cyan-200 p-5">
-        <div className="container mx-auto flex justify-between">
+      <nav className="bg-gradient-to-r from-blue-200 to-cyan-200">
+        <div className="container mx-auto flex justify-between p-5">
           <p className="text-xl font-bold">TaskSculpt</p>
           <ul className="flex  gap-x-5">
             <li>
@@ -29,13 +54,31 @@ export default function Layout({ children }: { children: React.ReactNode }) {
             <li>
               <IconBell />
             </li>
-            <li>
-              <IconUserCircle />
+            <li ref={ref} className="relative flex justify-center">
+              <IconUserCircle
+                onClick={handleOnMouseClick}
+                className="cursor-pointer"
+              />
+              {isDropdownVisible && (
+                <ul className="absolute top-8 flex flex-col items-center gap-y-4 bg-white p-5 shadow">
+                  <li className="cursor-pointer rounded p-3 font-semibold hover:bg-slate-50">
+                    Profile
+                  </li>
+                  <li
+                    className="cursor-pointer whitespace-nowrap rounded bg-red-400 p-3 font-semibold text-white hover:bg-red-500"
+                    onClick={() => {
+                      void signOut();
+                    }}
+                  >
+                    Sign out
+                  </li>
+                </ul>
+              )}
             </li>
           </ul>
         </div>
       </nav>
-      <main>{children}</main>
+      <main className="container mx-auto p-5">{children}</main>
     </>
   );
 }
