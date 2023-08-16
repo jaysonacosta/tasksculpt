9d072779a2c05514114e0a5a2bd00cb1c497f6b8
Jayson Acosta <36345453+jaysonacosta@users.noreply.github.com>
2023-08-16 13:59:34 -0400

    Created small layout and added calendar

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,17 +1,42 @@
 import Layout from "@/layout/Layout";
 import { Routes } from "@/types/routes";
-import { signOut, useSession } from "next-auth/react";
+import { useSession } from "next-auth/react";
 import Head from "next/head";
 import { useRouter } from "next/router";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
 
 export default function Dashboard() {
-  const { status } = useSession();
+  const { status, data } = useSession();
   const router = useRouter();
-  console.log(status);
+
   if (status === "unauthenticated") {
     void router.push(Routes.LOGIN);
   }
 
+  let content;
+
+  if (status === "loading") {
+    content = <h1>Loading...</h1>;
+  } else if (status === "authenticated") {
+    content = (
+      <>
+        <h1 className="text-2xl font-bold">Hi, {data.user.name}!</h1>
+        <br />
+        <section className="text-md grid gap-5 font-semibold md:grid-cols-3">
+          <div className="bg-white p-3 shadow md:col-start-1 md:col-end-3">
+            Overview
+          </div>
+          <div className="flex flex-col gap-y-3 bg-white p-3 shadow md:col-start-3 md:col-end-4">
+            <p>Calendar</p>
+            <Calendar calendarType="gregory" />
+          </div>
+          <div className="bg-white p-3 shadow">Todays tasks</div>
+          <div className="bg-white p-3 shadow">Upcoming assignments</div>
+        </section>
+      </>
+    );
+  }
   return (
     <>
       <Head>
@@ -19,7 +44,7 @@ export default function Dashboard() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Layout>Hi</Layout>
+      <Layout>{content}</Layout>
     </>
   );
 }
