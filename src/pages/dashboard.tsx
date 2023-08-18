75910dcb2ddf635be2b5b8e95c54d671ef6ecc66
Jayson Acosta <36345453+jaysonacosta@users.noreply.github.com>
2023-08-17 20:14:55 -0400

    Displaying tasks alongside color and status

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -82,8 +82,13 @@ export default function Dashboard() {
           {tasksDueTodayData.map((task) => {
             return (
               <div key={task.id} className="flex bg-slate-100">
-                <div className="flex w-full justify-between">
-                  <p className="p-2 font-semibold">{task.title}</p>
+                <div
+                  className="w-2"
+                  style={{ backgroundColor: task.course.colorCode }}
+                ></div>
+                <div className="flex w-full justify-between whitespace-nowrap">
+                  <p className="truncate p-2 font-semibold w-40">{task.title}</p>
+                  <p className="p-2 text-gray-600">{task.status}</p>
                 </div>
               </div>
             );
