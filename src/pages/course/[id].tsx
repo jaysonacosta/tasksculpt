2168cffe03e9f5307f3a64369d6d5ec949462735
Jayson Acosta <36345453+jaysonacosta@users.noreply.github.com>
2023-08-17 20:23:29 -0400

    Added react components for missing pages

---
+++ src/pages/course/[id].tsx
@@ -0,0 +1,17 @@
+import Layout from "@/layout";
+import Head from "next/head";
+
+export default function CoursePage() {
+  return (
+    <>
+      <Head>
+        <title>TaskSculpt | Course</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Layout>
+        <div>Course Page!</div>
+      </Layout>
+    </>
+  );
+}
