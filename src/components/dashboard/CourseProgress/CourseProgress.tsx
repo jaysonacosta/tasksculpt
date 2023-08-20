1ab2411ce4464c2826c6c80a34471685e195a3c4
Jayson Acosta <36345453+jaysonacosta@users.noreply.github.com>
2023-08-20 15:36:49 -0400

    Added pagination to course progress

--- src/components/dashboard/CourseProgress/CourseProgress.tsx
+++ src/components/dashboard/CourseProgress/CourseProgress.tsx
@@ -2,18 +2,32 @@ import { api } from "@/utils/api";
 import { IconExclamationCircle } from "@tabler/icons-react";
 import DashboardCard from "../DashboardCard";
 import TaskSkeleton from "../TaskSkeleton";
+import Pagination from "../Pagination";
+import { useState } from "react";
 
 export default function CourseProgress() {
   const {
     isSuccess,
-    data: courses,
     isLoading,
+    data,
+    fetchNextPage,
+    fetchPreviousPage,
     isError,
-  } = api.course.getAll.useQuery();
+    isFetching,
+  } = api.course.getAllPaginated.useInfiniteQuery(
+    {
+      limit: 5,
+    },
+    {
+      getNextPageParam: (lastPage) => lastPage.nextCursor,
+    }
+  );
+
+  const [page, setPage] = useState(0);
 
   let content;
 
-  if (isLoading) {
+  if (isLoading || isFetching) {
     content = <TaskSkeleton />;
   }
 
@@ -27,43 +41,59 @@ export default function CourseProgress() {
   }
 
   if (isSuccess) {
-    if (courses.length === 0) {
-      content = (
-        <div>
-          <p>
-            Nothing to see here. Add your first course and task to get started!
-          </p>
-        </div>
-      );
-    } else {
-      content = (
-        <>
-          {courses.map((course) => {
-            let taskInformation;
-            if (course.tasks.length === 0) {
-              taskInformation = "No tasks 🎉";
-            } else {
-              taskInformation = `${course.tasks.length} task${
-                course.tasks.length > 1 ? "s" : ""
-              }`;
-            }
-            return (
-              <div key={course.id} className="flex bg-slate-100">
-                <div
-                  className="w-2"
-                  style={{ backgroundColor: course.colorCode }}
-                ></div>
-                <div className="flex w-full justify-between">
-                  <p className="p-2 font-semibold">{course.courseName}</p>
-                  <p className="p-2 text-gray-600">{taskInformation}</p>
+    const { pages } = data;
+    const currentPage = pages[page];
+    if (currentPage) {
+      if (currentPage.courses.length === 0) {
+        content = (
+          <div>
+            <p>
+              Nothing to see here. Add your first course and task to get
+              started!
+            </p>
+          </div>
+        );
+      } else {
+        content = (
+          <>
+            {currentPage.courses.map((course) => {
+              let taskInformation;
+              if (course.tasks.length === 0) {
+                taskInformation = "No tasks 🎉";
+              } else {
+                taskInformation = `${course.tasks.length} task${
+                  course.tasks.length > 1 ? "s" : ""
+                }`;
+              }
+              return (
+                <div key={course.id} className="flex bg-slate-100">
+                  <div
+                    className="w-2"
+                    style={{ backgroundColor: course.colorCode }}
+                  ></div>
+                  <div className="flex w-full justify-between">
+                    <p className="p-2 font-semibold">{course.courseName}</p>
+                    <p className="p-2 text-gray-600">{taskInformation}</p>
+                  </div>
                 </div>
-              </div>
-            );
-          })}
-        </>
-      );
+              );
+            })}
+            <Pagination
+              pageCount={currentPage.pageCount}
+              page={page}
+              setPage={setPage}
+              fetchNextPage={fetchNextPage}
+              fetchPreviousPage={fetchPreviousPage}
+            />
+          </>
+        );
+      }
     }
   }
 
-  return <DashboardCard title="Course Progress">{content}</DashboardCard>;
+  return (
+    <DashboardCard title="Course Progress" className=" h-[344px]">
+      {content}
+    </DashboardCard>
+  );
 }
