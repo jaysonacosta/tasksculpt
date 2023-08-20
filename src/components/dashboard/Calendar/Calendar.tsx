dbdf92c382e5aec1bbdb332d3d516e2b84e022d7
Jayson Acosta <36345453+jaysonacosta@users.noreply.github.com>
2023-08-19 23:13:12 -0400

    Displaying number of tasks due on a given day on calendar

--- src/components/dashboard/Calendar/Calendar.tsx
+++ src/components/dashboard/Calendar/Calendar.tsx
@@ -1,13 +1,71 @@
 import ReactCalendar from "react-calendar";
 import DashboardCard from "../DashboardCard";
-import { type CalendarType } from "react-calendar/dist/cjs/shared/types";
+import {
+  type TileContentFunc,
+  type CalendarType,
+} from "react-calendar/dist/cjs/shared/types";
+import "react-calendar/dist/Calendar.css";
+import { api } from "@/utils/api";
+import { IconExclamationCircle, IconLoader } from "@tabler/icons-react";
 
 export default function Calendar() {
   const calendarType = Intl.DateTimeFormat().resolvedOptions()
     .calendar as CalendarType;
-  return (
-    <DashboardCard title="Calendar">
-      <ReactCalendar calendarType={calendarType} />
-    </DashboardCard>
-  );
+
+  const {
+    isSuccess,
+    data: tasks,
+    isLoading,
+    isError,
+  } = api.task.getAll.useQuery();
+
+  let content;
+
+  if (isLoading) {
+    content = (
+      <div>
+        <IconLoader className="animate-spin" color="#4299e1" />
+      </div>
+    );
+  }
+
+  if (isError) {
+    content = (
+      <div className="flex flex-col gap-y-5">
+        <IconExclamationCircle color="red" />
+        <p>Uh oh... Something went wrong...</p>
+      </div>
+    );
+  }
+
+  if (isSuccess) {
+    const populateCalendar: TileContentFunc = ({ date, view }) => {
+      if (view === "month") {
+        const initialTaskCount = 0;
+        const taskCount = tasks.reduce(
+          (acc, curr) =>
+            curr.dueDate.toISOString() === date.toISOString() ? acc + 1 : acc,
+          initialTaskCount
+        );
+
+        if (taskCount !== initialTaskCount) {
+          return (
+            <div className="absolute -right-1 -top-1 z-10 flex h-5 w-5 items-center justify-center rounded-full bg-emerald-400 text-xs">
+              <p className="font-semibold text-white">{taskCount}</p>
+            </div>
+          );
+        }
+      }
+    };
+
+    content = (
+      <ReactCalendar
+        tileContent={populateCalendar}
+        tileClassName={() => "!overflow-visible relative"}
+        calendarType={calendarType}
+      />
+    );
+  }
+
+  return <DashboardCard title="Calendar">{content}</DashboardCard>;
 }
