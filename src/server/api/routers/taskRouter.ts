562d9f36499e31043c28de5fb2b0162d23a4ca40
Jayson Acosta <36345453+jaysonacosta@users.noreply.github.com>
2023-08-20 19:09:29 -0400

    Added pagination to today's tasks

--- src/server/api/routers/taskRouter.ts
+++ src/server/api/routers/taskRouter.ts
@@ -8,20 +8,56 @@ export const taskRouter = createTRPCRouter({
       where: { userId: ctx.session.user.id },
     });
   }),
-  getAllDueToday: protectedProcedure.query(({ ctx }) => {
-    return ctx.prisma.task.findMany({
-      where: {
-        userId: ctx.session.user.id,
-        dueDate: {
-          lte: new Date(createDate().setHours(23, 59, 59, 999)),
-          gte: new Date(createDate().setHours(0, 0, 0, 0)),
+  getAllDueToday: protectedProcedure
+    .input(
+      z.object({
+        limit: z.number().min(1).max(10).nullish(),
+        cursor: z.string().nullish(),
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      const { limit, cursor } = input;
+      const paginationLimit = limit ?? 5;
+
+      const endOfDay = new Date(createDate().setHours(23, 59, 59, 999));
+      const beginningOfDay = new Date(createDate().setHours(0, 0, 0, 0));
+
+      const tasks = await ctx.prisma.task.findMany({
+        // Take extra item at end used as the next cursor
+        take: paginationLimit + 1,
+        where: {
+          userId: ctx.session.user.id,
+          dueDate: {
+            lte: endOfDay,
+            gte: beginningOfDay,
+          },
         },
-      },
-      include: {
-        course: true,
-      },
-    });
-  }),
+        include: {
+          course: true,
+        },
+        cursor: cursor ? { id: cursor } : undefined,
+      });
+
+      let nextCursor: typeof cursor | undefined;
+      if (tasks.length > paginationLimit) {
+        const nextItem = tasks.pop();
+        nextCursor = nextItem?.id;
+      }
+
+      const taskCount = await ctx.prisma.task.count({
+        where: {
+          userId: ctx.session.user.id,
+          dueDate: {
+            lte: endOfDay,
+            gte: beginningOfDay,
+          },
+        },
+      });
+
+      const pageCount = Math.ceil(taskCount / paginationLimit);
+
+      return { tasks, nextCursor, pageCount };
+    }),
   getAllUpcoming: protectedProcedure.query(({ ctx }) => {
     return ctx.prisma.task.findMany({
       where: {
