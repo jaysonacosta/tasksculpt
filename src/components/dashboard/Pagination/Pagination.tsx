61cf5f0d90a28a5dd3fc1e4c24e48bbdabaeaae4
Jayson Acosta <36345453+jaysonacosta@users.noreply.github.com>
2023-08-20 15:36:11 -0400

    Created component for pagination

---
+++ src/components/dashboard/Pagination/Pagination.tsx
@@ -0,0 +1,45 @@
+import { IconCircleArrowLeft, IconCircleArrowRight } from "@tabler/icons-react";
+import type { InfiniteQueryObserverResult } from "@tanstack/react-query";
+import type { Dispatch, SetStateAction } from "react";
+
+export default function Pagination({
+  pageCount,
+  page,
+  setPage,
+  fetchNextPage,
+  fetchPreviousPage,
+}: {
+  pageCount: number;
+  page: number;
+  setPage: Dispatch<SetStateAction<number>>;
+  fetchNextPage: () => Promise<InfiniteQueryObserverResult>;
+  fetchPreviousPage: () => Promise<InfiniteQueryObserverResult>;
+}) {
+  const nextPage = () => {
+    if (page < pageCount - 1) {
+      setPage((prev) => prev + 1);
+      void fetchNextPage();
+    }
+  };
+
+  const prevPage = () => {
+    if (page != 0) {
+      setPage((prev) => prev - 1);
+      void fetchPreviousPage();
+    }
+  };
+
+  return (
+    <div className="flex justify-around">
+      <button onClick={prevPage}>
+        <IconCircleArrowLeft />
+      </button>
+      <p>
+        {page + 1}/{pageCount}
+      </p>
+      <button onClick={nextPage}>
+        <IconCircleArrowRight />
+      </button>
+    </div>
+  );
+}
