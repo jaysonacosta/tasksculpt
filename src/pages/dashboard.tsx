b71065d5ac48a69ae8872b4f31079022868f9706
Jayson Acosta <36345453+jaysonacosta@users.noreply.github.com>
2023-08-19 20:34:09 -0400

    Replaced old instances of course progress logic with new component

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,4 +1,5 @@
 import Button from "@/components/Button/Button";
+import CourseProgress from "@/components/dashboard/CourseProgress";
 import TodaysTasks from "@/components/dashboard/TodaysTasks";
 import UpcomingTasks from "@/components/dashboard/UpcomingTasks";
 import Layout from "@/layout/Layout";
@@ -26,46 +27,6 @@ export default function Dashboard() {
     api.task.getAll.useQuery();
 
   let overview;
-  let courseProgress;
-
-  if (courseIsSuccess && taskIsSuccess) {
-    if (courseData.length === 0 && taskData.length === 0) {
-      overview = (
-        <div>
-          <p>
-            Nothing to see here. Add your first course and task to get started!
-          </p>
-        </div>
-      );
-    } else {
-      courseProgress = (
-        <>
-          {courseData.map((course) => {
-            let taskInformation;
-            if (course.tasks.length === 0) {
-              taskInformation = "No tasks 🎉";
-            } else {
-              taskInformation = `${course.tasks.length} task${
-                course.tasks.length > 1 ? "s" : ""
-              }`;
-            }
-            return (
-              <div key={course.id} className="flex bg-slate-100">
-                <div
-                  className="w-2"
-                  style={{ backgroundColor: course.colorCode }}
-                ></div>
-                <div className="flex w-full justify-between">
-                  <p className="p-2 font-semibold">{course.courseName}</p>
-                  <p className="p-2 text-gray-600">{taskInformation}</p>
-                </div>
-              </div>
-            );
-          })}
-        </>
-      );
-    }
-  }
 
   let content;
 
@@ -94,10 +55,7 @@ export default function Dashboard() {
           </div>
           <TodaysTasks />
           <UpcomingTasks />
-          <div className="flex flex-col gap-y-3 bg-white p-3 shadow">
-            <p className="font-semibold">Course Progress</p>
-            {courseProgress}
-          </div>
+          <CourseProgress />
         </div>
       </section>
     );
